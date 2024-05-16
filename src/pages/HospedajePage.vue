3bdc2ce41aa225cfdc58aee41eff909d9749d933
Hector Flores <52297636+chelexwe1812@users.noreply.github.com>
2024-05-16 03:19:53 +0000

    addHotelespage

--- src/pages/HospedajePage.vue
+++ src/pages/HospedajePage.vue
@@ -1,10 +1,22 @@
 <template>
-    <h4>Hospedaje</h4>
-    <li>lugar 1</li>
-    <li>lugar 1</li>
-    <li>lugar 1</li>
-    <li>lugar 1</li>
-    <div>
-        AQUI VIENE EL MAPA DE GOOGLE
-    </div>
+    <q-page>
+        <div class="bg-indigo-10 q-px-sm">
+            <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="blue-3"
+                label="HOTEL" />
+            <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="blue-3"
+                label="ALOJAMINETO" />
+            <q-btn unelevated class="full-width q-mb-sm" align="left" text-color="black" color="blue-3"
+                label="HOSTAL" />
+        </div>
+        <br />
+        <div class="row justify-end q-px-sm">
+            <q-btn rounded class="q-pa-md" color="pink-4" icon="arrow_back_ios" @click="$router.go(-1)" />
+        </div>
+    </q-page>
 </template>
+
+<script setup>
+defineOptions({
+    name: 'FixturePage'
+});
+</script>
